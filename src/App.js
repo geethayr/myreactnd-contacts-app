5dbb324757a2d006d27425b586596082dc4c5eb5
Geetha Yodham <geethayodham@gmail.com>
2020-05-07 13:14:41 -0500

    moved contacts to state

--- src/App.js
+++ src/App.js
@@ -3,33 +3,37 @@ import React, {Component} from 'react';
 import './App.css';
 import ListContacts from './ListContacts'
 
-const contacts = [
-    {
-        id: 'tyler',
-        name: 'Tyler McGinnis',
-        handle: '@tylermcginnis',
-        avatarURL: 'http://localhost:5001/tyler.jpg'
-    },
-    {
-        id: 'karen',
-        name: 'Karen Isgrigg',
-        handle: '@karen_isgrigg',
-        avatarURL: 'http://localhost:5001/karen.jpg'
-    },
-    {
-        id: 'richard',
-        name: 'Richard Kalehoff',
-        handle: '@richardkalehoff',
-        avatarURL: 'http://localhost:5001/richard.jpg'
-    },
-]
+
 
 class App extends Component {
+    state = {
+        contacts : [
+            {
+                id: 'tyler',
+                name: 'Tyler McGinnis',
+                handle: '@tylermcginnis',
+                avatarURL: 'http://localhost:5001/tyler.jpg'
+            },
+            {
+                id: 'karen',
+                name: 'Karen Isgrigg',
+                handle: '@karen_isgrigg',
+                avatarURL: 'http://localhost:5001/karen.jpg'
+            },
+            {
+                id: 'richard',
+                name: 'Richard Kalehoff',
+                handle: '@richardkalehoff',
+                avatarURL: 'http://localhost:5001/richard.jpg'
+            },
+        ]
+    }
+
     render () {
         return (
             <div>
                 Hello World
-                <ListContacts contacts={contacts}/>
+                <ListContacts contacts={this.state.contacts}/>
             </div>
         );
     }
