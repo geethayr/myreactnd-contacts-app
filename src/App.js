1bc212b4d17c65e8bfd2ebd0272326cea0c5bf4a
Geetha Yodham <geethayodham@gmail.com>
2020-05-07 13:09:04 -0500

    created List contacts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import ListContacts from './ListContacts'
+
+const contacts = [
+    {
+        id: 'tyler',
+        name: 'Tyler McGinnis',
+        handle: '@tylermcginnis',
+        avatarURL: 'http://localhost:5001/tyler.jpg'
+    },
+    {
+        id: 'karen',
+        name: 'Karen Isgrigg',
+        handle: '@karen_isgrigg',
+        avatarURL: 'http://localhost:5001/karen.jpg'
+    },
+    {
+        id: 'richard',
+        name: 'Richard Kalehoff',
+        handle: '@richardkalehoff',
+        avatarURL: 'http://localhost:5001/richard.jpg'
+    },
+]
 
 class App extends Component {
     render () {
         return (
             <div>
                 Hello World
+                <ListContacts contacts={contacts}/>
             </div>
         );
     }
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -2,11 +2,14 @@ import React, {Component} from 'react'
 
 class ListContacts extends Component {
     render() {
-        console.log('Props', this.props)
+       // console.log('Props', this.props)
         return (
             <ol className='contacts-list'>
-                {this.props.contacts.map((contact)}
-
+                {this.props.contacts.map((contact) => (
+                    <li key = {contact.id}>
+                        {contact.name}
+                    </li>
+                ))}
             </ol>
         );
     }
